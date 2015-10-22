5735d79929e8cff943f021be35892ff3b263a185
Siani <s.traynor@student.qut.edu.au>
2015-10-22 12:13:38 +1000

    ScheduledJobs AutoGenerated

---
+++ caapaService/ScheduledJobs/UserMapsJob.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace caapaService.ScheduledJobs
+{
+    // A simple scheduled job which can be invoked manually by submitting an HTTP
+    // POST request to the path "/jobs/UserMaps".
+
+    public class UserMapsJob : ScheduledJob
+    {
+        public override Task ExecuteAsync()
+        {
+            Services.Log.Info("Hello from scheduled job!");
+            return Task.FromResult(true);
+        }
+    }
+}
