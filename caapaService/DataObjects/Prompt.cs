df2dda28203b29b9dcc023d6a1bac1387e38cc31
Siani <s.traynor@student.qut.edu.au>
2015-10-25 00:47:51 +1000

    changes to prompt .cs data onjects

--- caapaService/DataObjects/Prompt.cs
+++ caapaService/DataObjects/Prompt.cs
@@ -13,6 +13,23 @@ namespace caapaService.DataObjects
         public String promptName { get; set; }
         public String promptDesc { get; set; }
         public int userId { get; set; }
+        
 
+        public Prompt()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CaaPa.Beacons"/> class.
+        /// </summary>
+        /// <param name="promptName">promptName .</param>
+        /// <param name="promptDesc">promptDesc .</param>
+        /// <param name="userId">userId .</param>
+        public Prompt(String promptName, String promptDesc, int userID)
+        {
+            this.promptName = promptName;
+            this.promptDesc = promptDesc;
+            this.userId = userId;
+        }
     }
 }
