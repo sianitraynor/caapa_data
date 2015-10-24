c78a9e87d3c09ce2dc585115614f95f94b8ea292
Siani <s.traynor@student.qut.edu.au>
2015-10-25 00:39:06 +1000

    data objects edits

---
+++ caapaService/DataObjects/Prompt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace caapaService.DataObjects
+{
+    class Prompt
+    {
+        public int promptId { get; set; }
+        public String promptName { get; set; }
+        public String promptDesc { get; set; }
+        public int userId { get; set; }
+
+    }
+}
---
+++ caapaService/DataObjects/Reminder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace caapaService.DataObjects
+{
+    class Reminder
+    {
+        public int ReminderId { get; set; }
+        public int UserId { get; set; }
+        public String ReminderName { get; set; }
+        public String ReminderDesc { get; set; }
+        public DateTime DurationStartDatetime { get; set; }
+        public DateTime DurationEndDatetime { get; set; }
+        public String ImageURI { get; set; }
+        public String MediaURI { get; set; }
+
+
+    }
+}
